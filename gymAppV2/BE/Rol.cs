44057c118b2cd7a1908336b74b9b03eb16d70799
Danunu <dperelmuter25@gmail.com>
2026-05-23 20:17:20 -0300

    feat: add Rol enum and update Usuario entity

---
+++ gymAppV2/BE/Rol.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace BE
+{
+    public enum Rol
+    {
+        Administrador = 1,
+        Recepcionista = 2,
+        Entrenador = 3,
+        Cliente = 4
+    }
+}
--- gymAppV2/BE/Usuario.cs
+++ gymAppV2/BE/Usuario.cs
@@ -4,11 +4,27 @@ namespace BE
 {
     public class Usuario
     {
-        public int USUARIO_Id { get; set; }
-        public string USUARIO_Nombre { get; set; }
+        public string USUARIO_Usuario { get; set; }
         public string USUARIO_Contras { get; set; }
+        public bool USUARIO_Activo { get; set; }
         public int USUARIO_Intentos { get; set; }
-        public bool USUARIO_Baja { get; set; }
-        public int USUARIO_DVH { get; set; }
+        public Rol USUARIO_Rol { get; set; }
+        public string USUARIO_DVV { get; set; }
+        public string USUARIO_DVH { get; set; }
+
+        public Usuario()
+        {
+        }
+
+        public Usuario(string usuario, string contras, bool activo, int intentos, Rol rol, string dvv, string dvh)
+        {
+            USUARIO_Usuario = usuario;
+            USUARIO_Contras = contras;
+            USUARIO_Activo = activo;
+            USUARIO_Intentos = intentos;
+            USUARIO_Rol = rol;
+            USUARIO_DVV = dvv;
+            USUARIO_DVH = dvh;
+        }
     }
 }
